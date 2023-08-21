fe2d9fdceb7434d917366815987745dd3c321904
Gustavo Dutra <dutragustavo@yahoo.com>
2023-08-20 21:50:27 -0300

    Adding binary search tree

--- arvores/support.py
+++ arvores/support.py
@@ -1,7 +1,7 @@
-from tree import BynaryTree, Node
+from tree import BinaryTree, Node
 
-if __name__ == "__main__":
-    tree = BynaryTree()
+def bynary_tree_ex():
+    tree = BinaryTree()
     n1 = Node('a')
     n2 = Node('+')
     n3 = Node('*')
@@ -23,3 +23,36 @@ if __name__ == "__main__":
 
     tree.root = n2
     tree.simetric_traversal()
+
+def postorder_tree_ex():
+    tree = BinaryTree()
+    n1 = Node('I')
+    n2 = Node('N')
+    n3 = Node('S')
+    n4 = Node('C')
+    n5 = Node('R')
+    n6 = Node('E')
+    n7 = Node('V')
+    n8 = Node('A')
+    n9 = Node('5')
+    n0 = Node('3')
+
+    n0.left = n6
+    n0.right = n9
+    n6.left = n1
+    n6.right = n5
+    n5.left = n2
+    n5.right = n4
+    n4.right = n3
+    n9.left = n8
+    n8.right = n7
+
+    tree.root = n0
+    return tree
+
+    
+if __name__ == "__main__":
+    tree = postorder_tree_ex()
+    print("Percurso em pós ordem:")
+    tree.postorder_traversal()
+    print("Altura: ", tree.height())
--- arvores/tree.py
+++ arvores/tree.py
@@ -5,9 +5,11 @@ class Node:
         self.right = None
 
 
-class BynaryTree:
-    def __init__(self, data=None):
-        if data:
+class BinaryTree:
+    def __init__(self, data=None, node=None):
+        if node:
+            self.root = node
+        elif data:
             node = Node(data)
             self.root = node
         else:
@@ -25,3 +27,63 @@ class BynaryTree:
             self.simetric_traversal(node.right)
             print(')', end='')
 
+
+    def postorder_traversal(self, node=None):
+        if node is None:
+            node = self.root
+        if node.left:
+            self.postorder_traversal(node.left)
+        if node.right:
+            self.postorder_traversal(node.right)
+        print(node.data)
+
+    def inorder_traversal(self, node=None):
+        if node is None:
+            node = self.root
+        if node.left:
+            self.inorder_traversal(node.left)
+        print(node.data, end=' ')
+        if node.right:
+            self.inorder_traversal(node.right)
+
+    def height(self, node=None):
+        if node is None:
+            node = self.root
+        hleft = 0
+        hright = 0
+        if node.left:
+            hleft = self.height(node.left)
+        if node.right:
+            hright = self.height(node.right)
+        if hright > hleft:
+            return hright + 1
+        return hleft + 1
+
+class BinarySearchTree(BinaryTree):
+    def insert(self, value):
+        parent = None
+        x = self.root
+        while(x):
+            parent = x
+            if value < x.data:
+                x = x.left
+            else:
+                x = x.right
+
+        if parent is None:
+            self.root = Node(value)
+        elif value < parent.data:
+            parent.left = Node(value)
+        else:
+            parent.right = Node(value)
+
+    def search(self, value, node=0):
+        if node == 0:
+            node = self.root
+        if node is None:
+            return node
+        if node.data == value:
+            return BinarySearchTree(node)
+        if value < node.data:
+            return self.search(value, node.left)
+        return self.search(value, node.right)
